d3d0e3487187688cf13914b846714a105c37462c
Nix <57319138@qq.com>
2019-04-07 22:28:35 +0800

    Test ECS

---
+++ Assets/Rotator.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    public float speed = 0;
+}
+
+class RotatorSystem : ComponentSystem {
+    protected override void OnUpdate() { 
+        var t = Time.deltaTime;
+        foreach(var e in GetEntities<Components>()) {
+            e.transform.Rotate(0, e.rotator.speed * t, 0);
+        }
+    }
+
+    struct Components {
+        public Rotator rotator;
+        public Transform transform;  
+    }
+}
